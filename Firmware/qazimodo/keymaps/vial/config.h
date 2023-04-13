227febc00b5cf2023d074d60bb9b816789ad65a8
calvin-mcd <114294748+calvin-mcd@users.noreply.github.com>
2023-04-13 02:40:49 +0200

    added firmware

---
+++ Firmware/qazimodo/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x07, 0x33, 0x66, 0x86, 0x9B, 0xAF, 0x96, 0xBF}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
+#define VIAL_INSECURE
